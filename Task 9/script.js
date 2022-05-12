bd011f8366e9f14e26db0715bfd4c35bfebc45c4
Julija Sauleniene <julija.sauleniene@gmail.com>
2022-05-12 16:57:17 +0300

    task 9 - added solution

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,15 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+const Movie = function(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+    this.wasExpensive = function() {
+        return this.budget > 100000000;
+    };
+};
+
+const test = new Movie("The Matrix", "Wachowski", 63000000);
+console.log(test);
+console.log(test.wasExpensive());
