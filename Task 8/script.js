03bc1b15275fc57fb149ca9ad7a54db246a37c70
Julija Sauleniene <julija.sauleniene@gmail.com>
2022-05-12 15:03:03 +0300

    task 8 - added solution

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,23 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator(a, b) {
+    this.a = a;
+    this.b = b;
+
+    sum = a + b;
+    substraction = a - b;
+    multiplication = a * b;
+    division = a/b;
+    
+    return {
+        'sum': sum,
+        'substraction': substraction,
+        'multiplication': multiplication,
+        'division': division
+    };
+};
+
+const test = new Calculator(10, 2);
+console.log(test);
